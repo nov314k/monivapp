f4701bccfd6ebca1e13fbb0e847aae5df711ddb8
Novak Petrovic <npetrovic@gmail.com>
2020-02-20 17:34:08 +0100

    Complete monivapp.sql

--- sql-scripts/monivapp.sql
+++ sql-scripts/monivapp.sql
@@ -1,79 +1,48 @@
-DROP DATABASE  IF EXISTS `spring_security_custom_user_demo`;
+DROP DATABASE IF EXISTS `monivapp`;
 
-CREATE DATABASE  IF NOT EXISTS `spring_security_custom_user_demo`;
+CREATE DATABASE IF NOT EXISTS `monivapp`;
+
 USE `spring_security_custom_user_demo`;
-
---
--- Table structure for table `user`
---
 
 DROP TABLE IF EXISTS `user`;
 
 CREATE TABLE `user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(50) NOT NULL,
-  `password` char(80) NOT NULL,
-  `first_name` varchar(50) NOT NULL,
-  `last_name` varchar(50) NOT NULL,
-  `email` varchar(50) NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(42) NOT NULL,
+  `password` char(84) NOT NULL,
+  `first_name` varchar(42) NOT NULL,
+  `last_name` varchar(42) NOT NULL,
+  `email` varchar(42) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `user`
---
--- NOTE: The passwords are encrypted using BCrypt
---
--- A generation tool is avail at: http://www.luv2code.com/generate-bcrypt-password
---
--- Default passwords here are: fun123
---
-
-INSERT INTO `user` (username,password,first_name,last_name,email)
-VALUES 
-('john','$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K','John','Doe','john@luv2code.com'),
-('mary','$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K','Mary','Public','mary@luv2code.com'),
-('susan','$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K','Susan','Adams','susan@luv2code.com');
-
-
---
--- Table structure for table `role`
---
+INSERT INTO `user` (username,password,first_name,last_name,email) VALUES 
+('voter','$2y$12$taX0i1nEwxLBFRbwgLLdde.2V5x55hp/rd8cOYD870Gxcq0qWH4Wm','One','Two','voter@example.com'),
+('maintainer','$2y$12$taX0i1nEwxLBFRbwgLLdde.2V5x55hp/rd8cOYD870Gxcq0qWH4Wm','Three','Four','maintainer@example.com'),
+('admin','$2y$12$taX0i1nEwxLBFRbwgLLdde.2V5x55hp/rd8cOYD870Gxcq0qWH4Wm','Five','Six','admin@example.com');
+('koliko','$2y$12$taX0i1nEwxLBFRbwgLLdde.2V5x55hp/rd8cOYD870Gxcq0qWH4Wm','Seven','Eight','koliko@example.com');
 
 DROP TABLE IF EXISTS `role`;
 
 CREATE TABLE `role` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) DEFAULT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(42) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `roles`
---
-
-INSERT INTO `role` (name)
-VALUES 
-('ROLE_EMPLOYEE'),('ROLE_MANAGER'),('ROLE_ADMIN');
-
---
--- Table structure for table `users_roles`
---
+INSERT INTO `role` (name) VALUES 
+('ROLE_VOTER'),('ROLE_MAINTAINER'),('ROLE_ADMIN');
 
 DROP TABLE IF EXISTS `users_roles`;
 
 CREATE TABLE `users_roles` (
-  `user_id` int(11) NOT NULL,
-  `role_id` int(11) NOT NULL,
-  
+  `user_id` int NOT NULL,
+  `role_id` int NOT NULL,  
   PRIMARY KEY (`user_id`,`role_id`),
-  
   KEY `FK_ROLE_idx` (`role_id`),
-  
   CONSTRAINT `FK_USER_05` FOREIGN KEY (`user_id`) 
   REFERENCES `user` (`id`) 
   ON DELETE NO ACTION ON UPDATE NO ACTION,
-  
   CONSTRAINT `FK_ROLE` FOREIGN KEY (`role_id`) 
   REFERENCES `role` (`id`) 
   ON DELETE NO ACTION ON UPDATE NO ACTION
@@ -81,14 +50,27 @@ CREATE TABLE `users_roles` (
 
 SET FOREIGN_KEY_CHECKS = 1;
 
---
--- Dumping data for table `users_roles`
---
-
-INSERT INTO `users_roles` (user_id,role_id)
-VALUES 
+INSERT INTO `users_roles` (user_id,role_id) VALUES
 (1, 1),
 (2, 1),
 (2, 2),
 (3, 1),
+(3, 2),
 (3, 3)
+
+DROP TABLE IF EXISTS `customer`;
+
+CREATE TABLE `customer` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(42) DEFAULT NULL,
+  `last_name` varchar(42) DEFAULT NULL,
+  `email` varchar(42) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+INSERT INTO `customer` VALUES 
+	(1,'Customer','Abc','abc@customer.com'),
+	(2,'Customer','Def','def@customer.com'),
+	(3,'Customer','Ghi','ghi@customer.com'),
+	(4,'Customer','Jkl','jkl@customer.com'),
+	(5,'Customer','Mno','mno@customer.com');
