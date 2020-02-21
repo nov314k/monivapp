d5205e2b20ff449703bb2fdd3ca7a41f517df99a
Novak Petrovic <npetrovic@gmail.com>
2020-02-21 12:04:41 +0100

    Refactor getActions() to getNumofRecentAdditions()

--- src/main/java/com/monivapp/dao/ActionDao.java
+++ src/main/java/com/monivapp/dao/ActionDao.java
@@ -1,13 +1,10 @@
 package com.monivapp.dao;
 
-import java.util.List;
-
 import com.monivapp.entity.Action;
 
 public interface ActionDao {
 
-	public List<Action> getActions();
+	public int getNumofRecentActions(String userName, String userAction, String fromDate);
 
-	public void saveAction(Action theAction);
-	
+	public void saveAction(Action theAction);	
 }
