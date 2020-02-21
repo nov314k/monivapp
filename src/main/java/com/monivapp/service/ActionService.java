cc87d71d496830a7bd7172e716f04b08f45b87c1
Novak Petrovic <npetrovic@gmail.com>
2020-02-21 12:05:41 +0100

    Rename getActions to getNumofRecentActions

--- src/main/java/com/monivapp/service/ActionService.java
+++ src/main/java/com/monivapp/service/ActionService.java
@@ -1,12 +1,10 @@
 package com.monivapp.service;
 
-import java.util.List;
-
 import com.monivapp.entity.Action;
 
 public interface ActionService {
-
-	public List<Action> getActions();
+	
+	public int getNumofRecentActions(String userName, String userAction, String fromDate);
 
 	public void saveAction(Action theAction);
 }
