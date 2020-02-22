c429b805e7b2bad822b65e46a8e5aac59786cb2a
Novak Petrovic <npetrovic@gmail.com>
2020-02-22 23:59:23 +0100

    Create

---
+++ src/main/java/com/monivapp/service/DetailServiceRestClientImpl.java
@@ -0,0 +1,31 @@
+package com.monivapp.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.monivapp.entity.Detail;
+
+@Service
+@PropertySource("classpath:application.properties")
+public class DetailServiceRestClientImpl implements DetailService {
+
+	private RestTemplate restTemplate;
+	private String restUrl;
+			
+	@Autowired
+	public DetailServiceRestClientImpl(RestTemplate theRestTemplate, 
+										@Value("${rest.url}") String theUrl) {
+		restTemplate = theRestTemplate;
+		restUrl = theUrl;
+	}
+
+	@Override
+	public Detail getDetail(String title) {
+		Detail theDetail = 
+				restTemplate.getForObject(restUrl + title, Detail.class);
+		return theDetail;
+	}
+}
