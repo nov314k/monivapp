97b6f73a43b939e0245d2d70ba3dee200273419c
Novak Petrovic <npetrovic@gmail.com>
2020-02-23 15:05:11 +0100

    Add getSearchResults, rename

--- src/main/java/com/monivapp/service/ApiServiceImpl.java
+++ src/main/java/com/monivapp/service/ApiServiceImpl.java
@@ -1,22 +1,29 @@
 package com.monivapp.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import com.monivapp.entity.Detail;
+import com.monivapp.entity.SearchResult;
 
 @Service
 @PropertySource("classpath:application.properties")
-public class DetailServiceImpl implements DetailService {
+public class ApiServiceImpl implements ApiService {
 
 	private RestTemplate restTemplate;
 	private String apiUrl;
 			
 	@Autowired
-	public DetailServiceImpl(RestTemplate theRestTemplate,
+	public ApiServiceImpl(RestTemplate theRestTemplate,
 			@Value("${omdb.api.url}") String theUrl) {
 		
 		restTemplate = theRestTemplate;
@@ -26,7 +33,20 @@ public class DetailServiceImpl implements DetailService {
 	@Override
 	public Detail getDetail(String title) {
 		
-		Detail theDetail = restTemplate.getForObject(apiUrl + title, Detail.class);
+		// TODO Extract "&t=" into the properties file
+		Detail theDetail = restTemplate.getForObject(apiUrl + "&t=" + title,
+				Detail.class);
 		return theDetail;
+	}
+	
+	@Override
+	public List<SearchResult> getSearchResults(String searchTerm) {
+
+		// TODO Extract "&s=" into the properties file
+		ResponseEntity<List<SearchResult>> responseEntity =
+				restTemplate.exchange(apiUrl + "&s=" + searchTerm, HttpMethod.GET, null,
+						new ParameterizedTypeReference<List<SearchResult>>() {});
+				List<SearchResult> searchResults = responseEntity.getBody();
+				return searchResults;
 	}
 }
