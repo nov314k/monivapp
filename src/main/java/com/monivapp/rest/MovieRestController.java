25c56656b0cc929372590697f5720a91fb740214
Novak Petrovic <npetrovic@gmail.com>
2020-02-24 01:19:42 +0100

    Add endpoints

--- src/main/java/com/monivapp/rest/MovieRestController.java
+++ src/main/java/com/monivapp/rest/MovieRestController.java
@@ -1,8 +1,17 @@
 package com.monivapp.rest;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,15 +21,55 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.monivapp.entity.Action;
 import com.monivapp.entity.Movie;
+import com.monivapp.service.ActionService;
 import com.monivapp.service.MovieService;
 
 @RestController
-@RequestMapping("/api") 
+@RequestMapping("/api")
+@PropertySource("classpath:application.properties")
 public class MovieRestController {
 	
 	@Autowired
 	private MovieService movieService;
+	
+	@Autowired
+	private ActionService actionService;
+	
+	@Autowired
+	private Environment env;
+	
+	private String keywordVoted;
+	private String keywordAdded;
+	private Authentication authentication;
+	private String currentPrincipalName;
+
+	private int numofRecentVotes;
+	private int numofRecentAdditions;
+	private int maxNumofAllowedRecentVotes;
+	private int numofRemainingVotes;
+	private int maxNumofAllowedRecentAdditions;
+	private int numofRemainingAdditions;
+	
+	@PostConstruct
+	protected void init() {
+
+		keywordVoted = env.getProperty("keyword.voted");
+		keywordAdded = env.getProperty("keyword.added");
+		
+		authentication = SecurityContextHolder.getContext().getAuthentication();
+		if (authentication == null) {
+			currentPrincipalName = env.getProperty("user.anonymous");
+		} else {
+			currentPrincipalName = authentication.getName();
+		}
+		
+		maxNumofAllowedRecentVotes = Integer.parseInt(
+				env.getProperty("maxNumofAllowedRecentVotes"));
+		maxNumofAllowedRecentAdditions = Integer.parseInt(
+				env.getProperty("maxNumofAllowedRecentAdditions"));
+	}
 	
 	@GetMapping("/movies")
 	public List<Movie> getMovies() {
@@ -41,8 +90,18 @@ public class MovieRestController {
 	@PostMapping("/movies")
 	public Movie addMovie (@RequestBody Movie theMovie) {
 		
-		theMovie.setId(0);
-		movieService.saveMovie(theMovie);
+		// NOTE No title checks are done here (some come from search, some assumed OK)
+		if (isAddingQuotaExceeded()) {
+			throw new MovieAddingQuotaException(
+					"You cannot add any more movies (quota exceeded)");
+		} else {
+			theMovie.setId(0);
+			theMovie.setVotes(0);
+			movieService.saveMovie(theMovie);
+			Action theAction = new Action(currentPrincipalName, keywordAdded,
+					theMovie.getId(), getTodaysDate());
+			actionService.saveAction(theAction);
+		}
 		return theMovie;
 	}
 	
@@ -50,6 +109,26 @@ public class MovieRestController {
 	public Movie updateMovie (@RequestBody Movie theMovie) {
 		
 		movieService.updateMovie(theMovie);
+		return theMovie;
+	}
+	
+	@GetMapping("/movies/vote/{movieId}")
+	public Movie voteForMovie (@PathVariable int movieId) {
+		
+		Movie theMovie = movieService.getMovie(movieId);
+		if (theMovie == null) {
+			throw new MovieNotFoundException("Movie id not found: " + movieId);
+		}
+		if (isVotingQuotaExceeded()) {
+			throw new MovieVotingQuotaException(
+					"You cannot cast any more votes (quota exceeded)");
+		} else {
+			movieService.vote(movieId);
+			Action theAction = new Action(currentPrincipalName, keywordVoted,
+					movieId, getTodaysDate());
+			actionService.saveAction(theAction);
+		}
+		theMovie = movieService.getMovie(movieId);
 		return theMovie;
 	}
 	
@@ -62,5 +141,52 @@ public class MovieRestController {
 		}
 		movieService.deleteMovie(movieId);
 		return "Deleted movie id: " + movieId; 
+	}
+	
+	// TODO Remove after testing
+	@GetMapping("/movies/reset")
+	public String resetActions() {
+		
+		actionService.resetActions();
+		return "Actions successfully cleared (quotas reset)";
+	}
+	
+	private String getFromDate() {
+		
+		Date currentDate = new Date();
+        Calendar c = Calendar.getInstance();
+        c.setTime(currentDate);
+        // TODO Extract time period out to the properties file (include DAYS also)
+        c.add(Calendar.MONTH, -1);
+        Date fromDate = c.getTime();
+        // TODO Extract date format out to the properties file
+        return new SimpleDateFormat("yyyy-MM-dd").format(fromDate);
+	}
+	
+	private String getTodaysDate() {
+		
+        // TODO Extract date format out to the properties file
+		return new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+	}
+	
+	private boolean isAddingQuotaExceeded() {
+		numofRecentAdditions = actionService.getNumofRecentActions(
+				currentPrincipalName, keywordAdded, getFromDate());
+		numofRemainingAdditions =
+				maxNumofAllowedRecentAdditions - numofRecentAdditions;
+		if (numofRemainingAdditions <= 0) {
+			return true;
+		}
+		return false;
+	}
+	
+	private boolean isVotingQuotaExceeded() {
+		numofRecentVotes = actionService.getNumofRecentActions(
+				currentPrincipalName, keywordVoted, getFromDate());
+		numofRemainingVotes = maxNumofAllowedRecentVotes - numofRecentVotes;
+		if (numofRemainingVotes <= 0) {
+			return true;
+		}
+		return false;
 	}
 }
