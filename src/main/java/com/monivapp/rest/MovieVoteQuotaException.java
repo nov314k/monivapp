5b03cc38af7e14c78b40b9166617ce6b0737211b
Novak Petrovic <npetrovic@gmail.com>
2020-02-25 20:39:39 +0100

    Rename

--- src/main/java/com/monivapp/rest/MovieVoteQuotaException.java
+++ src/main/java/com/monivapp/rest/MovieVoteQuotaException.java
@@ -1,25 +1,25 @@
 package com.monivapp.rest;
 
-public class MovieVotingQuotaException extends RuntimeException {
+public class MovieVoteQuotaException extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
 
-	public MovieVotingQuotaException() {
+	public MovieVoteQuotaException() {
 	}
 
-	public MovieVotingQuotaException(String arg0) {
+	public MovieVoteQuotaException(String arg0) {
 		super(arg0);
 	}
 
-	public MovieVotingQuotaException(Throwable arg0) {
+	public MovieVoteQuotaException(Throwable arg0) {
 		super(arg0);
 	}
 
-	public MovieVotingQuotaException(String arg0, Throwable arg1) {
+	public MovieVoteQuotaException(String arg0, Throwable arg1) {
 		super(arg0, arg1);
 	}
 
-	public MovieVotingQuotaException(String arg0, Throwable arg1, boolean arg2, boolean arg3) {
+	public MovieVoteQuotaException(String arg0, Throwable arg1, boolean arg2, boolean arg3) {
 		super(arg0, arg1, arg2, arg3);
 	}
 }
