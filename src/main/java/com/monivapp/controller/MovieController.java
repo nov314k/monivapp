e619bcf8c85e4842af66d569ad9bcb3a97f8549c
Novak Petrovic <npetrovic@gmail.com>
2020-02-26 13:33:54 +0100

    Add checks before saving and voting, add title as form parameter

--- src/main/java/com/monivapp/controller/MovieController.java
+++ src/main/java/com/monivapp/controller/MovieController.java
@@ -15,6 +15,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import com.monivapp.entity.Action;
 import com.monivapp.entity.Detail;
 import com.monivapp.entity.Movie;
+import com.monivapp.rest.MovieAddException;
+import com.monivapp.rest.MovieNotFoundException;
 import com.monivapp.service.ActionService;
 import com.monivapp.service.ApiService;
 import com.monivapp.service.MovieService;
@@ -59,29 +61,43 @@ public class MovieController {
 	}
 	
 	@PostMapping("/add")
-	public String saveMovie(@ModelAttribute("movie") Movie theMovie) {
+	public String saveMovie(Model theModel, @ModelAttribute("movie") Movie theMovie) {
 		
-		// TODO Add quote checks (cf REST controller)
-		// TODO Check for duplicate movies (titles)
-		
-		movieService.saveMovie(theMovie);
-		Action theAction = new Action(Helpers.getCurrentPrincipalName(),
-				Helpers.ACTION_ADDED, theMovie.getId(), Helpers.getTodaysDate());
-		actionService.saveAction(theAction);		
-		return "redirect:/movie/list";
+		if (Helpers.isAddingQuotaExceeded(actionService)) {
+			return "redirect:/movie/list";
+		} else if (theMovie.getTitle() == null || theMovie.getTitle() == "") {
+			theModel.addAttribute("errorMessage", "Please enter a movie title");
+			return "movie/addForm";
+		} else if (movieService.isTitleDuplicate(theMovie.getTitle())) {
+			theModel.addAttribute("errorMessage",
+					"Movie with the same title has already been suggested");
+			return "movie/addForm";
+		} else {
+			theMovie.setId(0);
+			theMovie.setVotes(0);
+			movieService.saveMovie(theMovie);
+			Action theAction = new Action(Helpers.getCurrentPrincipalName(),
+					Helpers.ACTION_ADDED, theMovie.getId(), Helpers.getTodaysDate());
+			actionService.saveAction(theAction);		
+			return "redirect:/movie/list";
+		}
 	}
 	
 	@GetMapping("/vote")
 	public String vote(@RequestParam("movieId") int theId) {
 		
-		// TODO Add quote checks (cf REST controller)
-		
-		movieService.vote(theId);
-		Action theAction = new Action(Helpers.getCurrentPrincipalName(),
-				Helpers.ACTION_VOTED, theId, Helpers.getTodaysDate());
-		actionService.saveAction(theAction);
-		return "redirect:/movie/list";
-	}	
+		if (!movieService.isIdValid(theId)) {
+			return "redirect:/movie/list";
+		} else if (Helpers.isVotingQuotaExceeded(actionService)) {
+			return "redirect:/movie/list";
+		} else {
+			movieService.vote(theId);
+			Action theAction = new Action(Helpers.getCurrentPrincipalName(),
+					Helpers.ACTION_VOTED, theId, Helpers.getTodaysDate());
+			actionService.saveAction(theAction);
+			return "redirect:/movie/list";
+		}
+	}
 	
 	@GetMapping("/delete")
 	public String deleteMovie(@RequestParam("movieId") int theId) {
@@ -115,9 +131,10 @@ public class MovieController {
 	}
 	
 	@GetMapping("/addForm")
-	public String showAddForm(Model theModel) {
+	public String showAddForm(Model theModel, @RequestParam("title") String title) {
 		
 		Movie theMovie = new Movie();
+		theMovie.setTitle(title);
 		theModel.addAttribute("movie", theMovie);
 		return "movie/addForm";
 	}
